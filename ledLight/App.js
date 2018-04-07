bf503f80f6c9a690028e23f4e386998b43d77076
Holly Vaccarella <hollyvacc@gmail.com>
2018-04-07 01:43:57 -0400

    Added Wallpaper.js and HomeScreen.js and edited App.js

--- ledLight/App.js
+++ ledLight/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeScreen from './HomeScreen';
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//         <Text>asdasfgfognsdkl</Text>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'purple'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'orange'}} />
+//       </View>
+      
+//     );
+//   }
+// }
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Dashboard: {
+      screen: DashScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>asdasfgfognsdkl</Text>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'purple'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'orange'}} />
-      </View>
-      
-    );
+    return <RootStack />;
   }
-}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -27,3 +48,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
---
+++ ledLight/HomeScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Button, View, Text } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Wallpaper from './Wallpaper';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <Wallpaper>
+      </Wallpaper>
+    );
+  }
+}
