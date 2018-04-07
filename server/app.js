9622a275f074edb88957fc1bc469f96b910f13a2
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2018-04-06 22:00:40 -0400

    not using stormpath

--- server/app.js
+++ server/app.js
@@ -18,4 +18,4 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('running at http://' + host + ':' + port)
-})
+});
