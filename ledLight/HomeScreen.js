a7451e876f304ec54d55b0f6a4e832bcf691c6f9
Holly Vaccarella <hollyvacc@gmail.com>
2018-04-07 03:06:17 -0400

    Added the button to HomeScreen.js

--- ledLight/HomeScreen.js
+++ ledLight/HomeScreen.js
@@ -1,14 +1,44 @@
 import React, {Component} from 'react';
-import { Button, View, Text } from 'react-native';
+import { Alert, Button, View, Text, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Wallpaper from './Wallpaper';
 
 export default class HomeScreen extends React.Component {
+
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
   render() {
     return (
       <Wallpaper>
-        
+        <View style={styles.buttonContainer}>
+          <Button 
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
       </Wallpaper>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20,
+    height: 50,
+    backgroundColor: "white",
+    justifyContent: 'center'
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
