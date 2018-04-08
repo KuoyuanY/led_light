05997fbd2064747f7182e69153f3b8004da9ea83
Holly Vaccarella <hollyvacc@gmail.com>
2018-04-07 22:22:29 -0400

    Changes

--- ledLight/DashScreen.js
+++ ledLight/DashScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button, View, Text, Dimensions, StyleSheet, Picker } from 'react-native';
+import { Button, View, Text, Dimensions, StyleSheet, Picker, TouchableOpacity } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { ColorWheel } from 'react-native-color-wheel';
 import Wallpaper from './Wallpaper';
@@ -11,7 +11,11 @@ export default class DashScreen extends React.Component {
   
 	constructor(props) {
     	super(props);
-    	this.state = {language: ''};
+    	this.state = {effect: 'solid',
+    				  colorR : 0,
+    				  colorG : 0,
+    				  colorB : 0,
+    				  switch : "OFF"};
 
   	}
 
@@ -23,55 +27,20 @@ export default class DashScreen extends React.Component {
 	      <Wallpaper>
 		      <View style={{flex: 3, marginTop:50}}>
 		        <ColorWheel
-		          initialColor="#000000"
+		          initialColor="#FFFFFF"
 		          onColorChange={color => 
 					
 					{if (color.h < 0) {
 						let colour = convert.hsv.rgb(360-color.h*(-1), color.s, color.v);
-						let args = {};
-					    args.state = "ON";
-					    args.colorR = colour[0];
-					    args.colorG = colour[1];
-					    args.colorB = colour[2];
-					    args.brightness = 50;
-					    args.effect = "solid";
-					    args.switches = "yes";
-					    args.setCol = "yes";
-					    args.setBr = "yes";
-
-						fetch('http://localhost:3000/set', {
-						  method: 'POST',
-						  headers: {
-						    Accept: 'application/json',
-						    'Content-Type': 'application/json',
-						  },
-						  body: JSON.stringify(
-						    args
-						  ),
-						});
+						this.setState({colorR : colour[0],
+									   colorG : colour[1],
+									   colorB : colour[2]})
 					} else {
 						let colour = convert.hsv.rgb(color.h, color.s, color.v);
-						let args = {};
-					    args.state = "ON";
-					    args.colorR = colour[0];
-					    args.colorG = colour[1];
-					    args.colorB = colour[2];
-					    args.brightness = 50;
-					    args.effect = "solid";
-					    args.switches = "yes";
-					    args.setCol = "yes";
-					    args.setBr = "yes";
-
-						fetch('http://localhost:3000/set', {
-						  method: 'POST',
-						  headers: {
-						    Accept: 'application/json',
-						    'Content-Type': 'application/json',
-						  },
-						  body: JSON.stringify(
-						    args
-						  ),
-						});
+						this.setState({colorR : colour[0],
+									   colorG : colour[1],
+									   colorB : colour[2]})
+						
 					}}}
 		          style={{width: Dimensions.get('window').width}}
 		          thumbStyle={{ height: 30, width: 30, borderRadius: 30}} />
@@ -79,9 +48,10 @@ export default class DashScreen extends React.Component {
 		      </View>
 		      <View style={{flex: 2, marginTop: 40, backgroundColor: 'rgba(255,255,255,0.1)'}}> 
 		      		<Picker
-						selectedValue={this.state.language}
+						selectedValue={this.state.effect}
 					 
-				    	onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+				    	onValueChange={(itemValue, itemIndex) => this.setState({effect: itemValue})}>
+
 				    	<Picker.Item label="solid" value="solid"/>
 						<Picker.Item label="bpm" value="bpm"/>
 						<Picker.Item label="candy cane" value="candy cane"/>
@@ -103,10 +73,124 @@ export default class DashScreen extends React.Component {
 
 						</Picker>
 			   </View>
-			   <View style={{flex: 1}}> </View>
+			   <TouchableOpacity onPress={() => 
+			   		{let args = {};
+				    args.state = "ON";
+				    this.setState({switch : "ON"});
+				    args.colorR = this.state.colorR;
+				    args.colorG = this.state.colorG;
+				    args.colorB = this.state.colorB;
+				    args.brightness = 50;
+				    args.effect = this.state.effect;
+				    args.switches = "yes";
+				    args.setCol = "yes";
+				    args.setBr = "yes";
+
+					fetch('http://localhost:3000/set', {
+					  method: 'POST',
+					  headers: {
+					    Accept: 'application/json',
+					    'Content-Type': 'application/json',
+					  },
+					  body: JSON.stringify(
+					    args
+					  ),
+					});}}
+			   	>
+            <View style={styles.buttonContainer}>
+              <Text style= {styles.buttonText}>Submit</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => 
+          	{if (this.state.switch == "OFF") {
+          		this.setState({switch: "ON"});
+          		let args = {};
+				    args.state = "ON";
+				    args.colorR = this.state.colorR;
+				    args.colorG = this.state.colorG;
+				    args.colorB = this.state.colorB;
+				    args.brightness = 50;
+				    args.effect = this.state.effect;
+				    args.switches = "yes";
+				    args.setCol = "yes";
+				    args.setBr = "yes";
+
+					fetch('http://localhost:3000/set', {
+					  method: 'POST',
+					  headers: {
+					    Accept: 'application/json',
+					    'Content-Type': 'application/json',
+					  },
+					  body: JSON.stringify(
+					    args
+					  ),
+					});
+          	} else {
+          		this.setState({switch: "OFF"});
+          		let args = {};
+				    args.state = "OFF";
+				    args.colorR = this.state.colorR;
+				    args.colorG = this.state.colorG;
+				    args.colorB = this.state.colorB;
+				    args.brightness = 50;
+				    args.effect = this.state.effect;
+				    args.switches = "yes";
+				    args.setCol = "yes";
+				    args.setBr = "yes";
+
+					fetch('http://localhost:3000/set', {
+					  method: 'POST',
+					  headers: {
+					    Accept: 'application/json',
+					    'Content-Type': 'application/json',
+					  },
+					  body: JSON.stringify(
+					    args
+					  ),
+					});
+          	}}}>
+
+          	<View style={styles.buttonContainer}>
+              <Text style= {styles.buttonText}>Power</Text>
+            </View>
+          </TouchableOpacity>
+		  <View style={{flex: 1}}> </View>
 	      </Wallpaper>
 	    );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    margin: 10,
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 30
+  },
+  buttonText: {
+    margin: 10,
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 25
+  },
+  buttonContainer: {
+    margin: 10,
+    height: 50,
+    backgroundColor: "white",
+    justifyContent: 'center',
+    borderRadius: 25,
+  },
+  logo: {
+    width: 193, 
+    height: 110,
+
+  }
+})
 
 
